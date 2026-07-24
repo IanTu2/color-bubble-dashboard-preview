8a995355062291802aa0ac4a2548108307fedac3
IanTu2 <wenchang10802270@gmail.com>
2026-07-24 10:57:26 +0800

    style: load complete todo manager styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './auth.css'
 import './member-layout.css'
+import './todo-manager.css'
 
 const rootElement = document.getElementById('root')
 
