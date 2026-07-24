c08a3ddfe9b06ce2044871d431fc0c12dd44f434
IanTu2 <wenchang10802270@gmail.com>
2026-07-24 14:24:24 +0800

    style: load multi-window desktop stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './styles.css'
 import './auth.css'
 import './member-layout.css'
 import './todo-manager.css'
-import './workspace.css'
+import './desktop-workspace.css'
 import './music-studio.css'
 
 const rootElement = document.getElementById('root')
