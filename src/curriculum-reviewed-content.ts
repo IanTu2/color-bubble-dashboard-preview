8714f1d52c3974b377b271a32053fd492ae1036c
IanTu2 <wenchang10802270@gmail.com>
2026-08-11 12:27:13 +0800

    feat: register reviewed grade 7 science content

--- src/curriculum-reviewed-content.ts
+++ src/curriculum-reviewed-content.ts
@@ -9,9 +9,12 @@ export type {
 
 import { getReviewedUnitContent as getSocial10UnitContent } from './curriculum-reviewed-social10'
 import { getReviewedMath7UnitContent } from './curriculum-reviewed-math7'
+import { getReviewedScience7UnitContent } from './curriculum-reviewed-science7'
 
 export function getReviewedUnitContent(unitId: string) {
-  return getSocial10UnitContent(unitId) ?? getReviewedMath7UnitContent(unitId)
+  return getSocial10UnitContent(unitId)
+    ?? getReviewedMath7UnitContent(unitId)
+    ?? getReviewedScience7UnitContent(unitId)
 }
 
 export function isReviewedUnit(unitId: string) {
