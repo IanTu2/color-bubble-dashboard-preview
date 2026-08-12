fea4fcc1f1fee33257478e4b21ff6720a5ff5508
IanTu2 <wenchang10802270@gmail.com>
2026-08-12 15:35:47 +0800

    fix: reset curriculum player when route changes

--- src/components/CurriculumCourseApp.tsx
+++ src/components/CurriculumCourseApp.tsx
@@ -1,4 +1,17 @@
-export {
-  CurriculumCourseApp,
+import {
+  CurriculumCourseApp as CurriculumCourseAppV13,
   type CurriculumCourseSelection,
 } from './CurriculumCourseAppV13'
+import type { Language } from '../types'
+
+export type { CurriculumCourseSelection }
+
+type Props = CurriculumCourseSelection & {
+  language: Language
+  userId: string
+}
+
+export function CurriculumCourseApp(props: Props) {
+  const routeKey = `${props.grade}-${props.subject}-${props.pathway ?? 'base'}`
+  return <CurriculumCourseAppV13 key={routeKey} {...props} />
+}
